01159a77780d503ac8f72c54ee76acfac3b1182a
yycer <15900617310@163.com>
2022-04-15 20:25:50 +0800

    First Commit.

---
+++ src/main/java/com/example/javaspecification/JavaSpecificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaspecification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpecificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpecificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaspecification/JavaSpecificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspecification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpecificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
